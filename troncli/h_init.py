39e62d7280c6d96fe8b97e063872705150a7fd0b
Ujwal Battar <ujwal.battar@gmail.com>
2019-03-11 12:20:03 -0700

    change conditional in init to use lates version

--- troncli/h_init.py
+++ troncli/h_init.py
@@ -108,10 +108,7 @@ class Init(object):
             url += 'Odyssey-v' + version
             self.source_full_jar = 'java-tron.jar'
             await self.node_list.update_node_version(version)
-        elif '3.2.0' <= version <= '3.2.10':
-            url += 'Odyssey-v' + version
-            await self.node_list.update_node_version(version)
-        elif '3.2.11' <= version < JAVA_TRON_LASTEST_VERSION:
+        elif '3.2.0' <= version < JAVA_TRON_LASTEST_VERSION:
             url += 'Odyssey-v' + version
             await self.node_list.update_node_version(version)
         else:
